626ec5122f7664730f4c65cf2f0fe121851961af
DanielVital <daniel@quadrant.pe>
2022-11-20 02:41:44 -0500

    routing set

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,23 @@
-import Item from './../list/Components/Item';
-import Details from './../itemDetail/components/Details';
-import List from './../list/List';
-import Booking from '../booking/Booking';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import ListPage from '../list';
+import DetailedPage from '../itemDetail';
+import BookingPage from '../booking';
 
-const arr = [
-<Item img="https://st2.depositphotos.com/1000165/5463/i/450/depositphotos_54636005-stock-photo-two-skateboards.jpg" nombre="pan" precio="132"/>,
-<Item img="https://img.freepik.com/psd-gratis/patinetas-colores-piso_23-2148272654.jpg?w=1380&t=st=1666259797~exp=1666260397~hmac=1dce4cb2d3326d015575214154ab38d199e6d26e836505f31494e1be06339817" nombre="pan" precio="132"/>,
-<Item img="https://i.pinimg.com/564x/e4/6e/4a/e46e4a9a5c58dc181cf15bb1ea8849cf.jpg" nombre="pan" precio="132"/>,
-<Item img="https://i.pinimg.com/564x/96/ca/a9/96caa961966c7c41329e31ab5fc58b9a.jpg" nombre="pan" precio="132"/>,
-<Item img="https://i.pinimg.com/564x/3c/3f/87/3c3f87cd793adb5005e9d8ea7d3df9b2.jpg" nombre="pan" precio="132"/>,
-<Item img="https://i.pinimg.com/564x/80/37/fb/8037fb2d3e0cf616d27165fc6f00f15b.jpg" nombre="pan" precio="132"/>
-]
-const item = {
-  nombre: "aaa",
-  img: "https://i.pinimg.com/564x/80/37/fb/8037fb2d3e0cf616d27165fc6f00f15b.jpg"
-}
+
+
 function App() {
   return (
-    <List arr={arr}/>
-    //<Details img="https://i.pinimg.com/564x/96/ca/a9/96caa961966c7c41329e31ab5fc58b9a.jpg" nombre="MOTOCICLETA HIDRAULICA" precio="132"/>
-    //<Booking item={item}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListPage/>} />
+        <Route path="/:id" element={<DetailedPage/>} />
+        <Route path="/pays" element={<BookingPage/>} />
+      </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ src/itemDetail/index.js
@@ -0,0 +1,10 @@
+import Details from './../itemDetail/components/Details';
+
+import React from 'react'
+
+const DetailedPage = () => {
+  return (
+<Details img="https://i.pinimg.com/564x/96/ca/a9/96caa961966c7c41329e31ab5fc58b9a.jpg" nombre="MOTOCICLETA HIDRAULICA" precio="132"/>  )
+}
+
+export default DetailedPage
