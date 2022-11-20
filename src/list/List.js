881abfbc16eb995f01307033c9e324bb8b0e3c4d
DanielVital <daniel@quadrant.pe>
2022-11-20 13:08:33 -0500

    apicalls itemlist

--- src/list/List.js
+++ src/list/List.js
@@ -3,14 +3,16 @@ import NavBar from '../common/NavBar';
 import ItemList from './Components/ItemList';
 
 
- const List = ({arr}) => {
+
+
+ const List = ({data}) => {
     return (
         <div>
           <div className="flex flex-col h-full w-full bg-indigo-50 justify-end items-start">
           <NavBar/>
           </div>
           <div className="flex flex-col h-full w-full bg-indigo-50 justify-end items-start">
-           <ItemList Items= {arr }/>
+           <ItemList data= {data }/>
            </div>
         </div>
       );
--- src/list/api/listapi.js
+++ src/list/api/listapi.js
@@ -1,46 +1,46 @@
-const axios = require('axios').default;
+import axios from 'axios'
+
 const arr ={
         list: [
             {
                 id:1,
                  img: "https://st2.depositphotos.com/1000165/5463/i/450/depositphotos_54636005-stock-photo-two-skateboards.jpg",
-                 nombre: "skate generico negro",
-                 precio: "120"
+                 name: "skate generico negro",
+                 price: "120"
             },
             {
                 id:2,
                  img: "https://img.freepik.com/psd-gratis/patinetas-colores-piso_23-2148272654.jpg?w=1380&t=st=1666259797~exp=1666260397~hmac=1dce4cb2d3326d015575214154ab38d199e6d26e836505f31494e1be06339817",
-                 nombre: "skate generico diseño ciudad",
-                 precio: "150"
+                 name: "skate generico diseño ciudad",
+                 price: "150"
             },            {
                 id:3,
                  img: "https://st2.depositphotos.com/1000165/5463/i/450/depositphotos_54636005-stock-photo-two-skateboards.jpg",
-                 nombre: "skate generico negro",
-                 precio: "120"
+                 name: "skate generico negro",
+                 price: "120"
             },            {
                 id:4,
                  img: "https://st2.depositphotos.com/1000165/5463/i/450/depositphotos_54636005-stock-photo-two-skateboards.jpg",
-                 nombre: "skate generico negro",
-                 precio: "120"
+                 name: "skate generico negro",
+                 price: "120"
             }, 
             {
                 id:5,
                  img: "https://st2.depositphotos.com/1000165/5463/i/450/depositphotos_54636005-stock-photo-two-skateboards.jpg",
-                 nombre: "skate generico negro",
-                 precio: "120"
+                 name: "skate generico negro",
+                 price: "120"
             },            {
                 id:6,
                  img: "https://st2.depositphotos.com/1000165/5463/i/450/depositphotos_54636005-stock-photo-two-skateboards.jpg",
-                 nombre: "skate generico negro",
-                 precio: "120"
+                 name: "skate generico negro",
+                 price: "120"
             }
         ]
     };
     
 const GetList = async() =>{
-    const response = await axios.get('url');
-    return response == undefined? response: arr;
+    return arr;
 } 
 
-    export default GetList
+ export default GetList
     
